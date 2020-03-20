efc3772b07968d7bc06642da6db018de34ad6280
Ryan Kaplan <ryankaplan7@gmail.com>
2020-03-20 15:17:08 -0400

    added basic html and get method, still having issues with post

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+// LOOK AT DAYPLANNER ACTIVITY************* 
+
 // Dependencies
 var express = require("express");
 var exphbs = require("express-handlebars");
+var mysql = require("mysql");
 
 // Create an instance of the express app.
 var app = express();
@@ -9,15 +12,52 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080;
 
-// Set Handlebars as the default templating engine.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Data
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+});
 
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
 
+    console.log("connected as id " + connection.threadId);
+});
 // Routes
+// Use Handlebars to render the main index.html page with the burgers in it.
+app.get("/", function (req, res) {
+    connection.query("SELECT * FROM burgers;", function (err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
 
+        res.render("index", { burgers: data });
+    });
+});
+
+// Create a new burger
+app.post("/api/burgers", function (req, res) {
+    connection.query("INSERT INTO burgers (burger_name, devoured) VALUES (?)", [req.body.burger_name, req.body.devoured], function (err, result) {
+        if (err) {
+            return res.status(500).end();
+        }
+
+        // Send back the ID of the new movie
+        res.json({ id: result.insertId });
+        console.log({ id: result.insertId });
+    });
+});
 
 
 // Start our server so that it can begin listening to client requests.
