c65abed4c737c17dcb36b3dc5727fb83b9d71d95
Ryan Kaplan <ryankaplan7@gmail.com>
2020-03-24 14:53:05 -0400

    updated env

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,10 +12,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: process.env.DB_HOST,
         port: 3306,
-        user: "root",
-        password: "root",
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         database: "burgers_db"
     });
 };
