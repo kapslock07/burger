c9ee4131131c27c42a4f648bde1025dbde72f490
Ryan Kaplan <ryankaplan7@gmail.com>
2020-03-24 14:09:42 -0400

    added heroku code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 var mysql = require("mysql");
 const dotenv = require('dotenv').config()
+var connection;
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,13 +6,19 @@ var mysql = require("mysql");
 
 const dotenv = require('dotenv').config()
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
 
 const burger = require("../models/burger");
 
